2be855e9a33553f85083c3bb903d14516b52025c
Pierre R Vernier <pierre.vernier@gmail.com>
2020-04-07 11:38:31 -0700

    Dropped code folder

--- code/figures_shiny/global.R
+++
@@ -1,9 +0,0 @@
-library(sf)
-library(shiny)
-library(tidyverse)
-
-sppList = c("all_birds","for_birds","caribou","boch","brcr","btnw","cawa","cmwa","pigr","rubl","swth","wwcr")
-ecor = st_read("data/pba_ecor.shp")
-ecop = st_read("data/pba_ecop.shp")
-ecoz = st_read("data/pba_ecoz.shp")
-
