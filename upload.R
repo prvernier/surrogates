6f87b175fb8a33caeb617b1cbef0bf548c0bcf11
Pierre R Vernier <pierre.vernier@gmail.com>
2020-05-01 12:25:57 -0700

    Updates to table_s1.md

---
+++ upload.R
@@ -0,0 +1,6 @@
+rsconnect::setAccountInfo(name='prvernier', 
+	token='69CE3122492E8BCE1CF26C3EE8AE8B74', 
+	secret='9WhdcUk0L5rVwW4YaIoDOEcYlh+adsIYGt3MR1fx')
+
+library(rsconnect)
+rsconnect::deployApp('surrogates')
