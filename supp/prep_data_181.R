022efc93fcbcc98741fb2eaf0a13ea3a7cbbe446
Pierre R Vernier <pierre.vernier@gmail.com>
2021-05-17 10:47:11 -0700

    Updated case studies

--- supp/prep_data_181.R
+++ supp/prep_data_181.R
@@ -1,5 +1,5 @@
 # Test representation analysis (BC Albers projection)
-# PV 2021-04-13
+# PV 2021-04-24
 
 library(sf)
 library(raster)
@@ -16,18 +16,18 @@ bnd1 = fasterize(sf=fda, raster=raster(fda, res=1000), field="one")
 
 # List of rasters to use and directory name
 raster1000 = c('cmi','gpp','led','lcc')
-dir1000 = '../../../gisdata/canada/raster1000/'
+dir1000 = '../gisdata/canada/raster1000/'
 # Generate data for study region
 for (i in raster1000) {
     print(i); flush.console()
     r = raster(paste0(dir1000,i,'.tif'))
-    if (i=='lcc') {
+    if (i %in% c('lcc','dhi14','dhi40')) {
         r = projectRaster(from=r, to=bnd1, res=1000, crs=crs(bnd1), method='ngb')
     } else {
         r = projectRaster(from=r, to=bnd1, res=1000, crs=crs(bnd1), method='bilinear')
     }
     r = mask(r, bnd1)
-    writeRaster(r, paste0('eco181/', i, '.tif'), overwrite=T)
+    writeRaster(r, paste0('data/eco181/', i, '.tif'), overwrite=T)
 }
 
 
@@ -38,13 +38,14 @@ for (i in raster1000) {
 bnd4 = fasterize(sf=fda, raster=raster(fda, res=4000), field="one")
 
 # Bird groups (canada albers)
-guilds = list.files('../../../github/surrogates/data/songbirds_groups/')
+guilds = list.files('../github/surrogates/data/songbirds_groups/')
 guilds = substr(guilds, 1, nchar(guilds)-4)
 for (i in guilds) {
-    r <- raster(paste0('../../../github/surrogates/data/songbirds_groups/',i,'.tif'))
+    print(i); flush.console()
+    r <- raster(paste0('../github/surrogates/data/songbirds_groups/',i,'.tif'))
     r <- projectRaster(from=r, to=bnd4, res=4000, crs=crs(bnd4), method='bilinear')
     r <- mask(r, bnd4)
-    writeRaster(r, paste0('eco181/',i,'.tif'), overwrite=T)
+    writeRaster(r, paste0('data/eco181/',i,'.tif'), overwrite=T)
 }
 
 # Birds from case study (canada albers)
--- supp/run.R
+++ supp/run.R
@@ -1,2 +1,2 @@
 rmarkdown::render("case_study.Rmd", output_file="case_study_89.html", params=list(eco=89))
-rmarkdown::render("case_study.Rmd", output_file="case_study_181.html", params=list(eco=181))
+#rmarkdown::render("case_study.Rmd", output_file="case_study_181.html", params=list(eco=181))
